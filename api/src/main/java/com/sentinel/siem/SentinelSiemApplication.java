646e1d01ce88792af6c8dc313588c0d1a5d0e291
berkekeceli <berke.keceli@ikmbilisim.com.tr>
2024-12-18 14:32:49 +0300

    add: Backend Template

---
+++ api/src/main/java/com/sentinel/siem/SentinelSiemApplication.java
@@ -0,0 +1,12 @@
+package com.sentinel.siem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelSiemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelSiemApplication.class, args);
+    }
+}
---
+++ api/src/test/java/com/sentinel/siem/SentinelSiemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinel.siem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelSiemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
